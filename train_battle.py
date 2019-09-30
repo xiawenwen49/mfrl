5f2e9f6615b63713a0f2ddb2f4796611f5f82034
xiawenwen <xiawenwen49@gmail.com>
2019-09-30 09:33:49 +0800

    add {}

--- train_battle.py
+++ train_battle.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     tf_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
     tf_config.gpu_options.allow_growth = True
 
-    log_dir = os.path.join(BASE_DIR,'data/tmp'.format(args.algo))
+    log_dir = os.path.join(BASE_DIR,'data/tmp/{}'.format(args.algo))
     model_dir = os.path.join(BASE_DIR, 'data/models/{}'.format(args.algo))
 
     if args.algo in ['mfq', 'mfac']:
